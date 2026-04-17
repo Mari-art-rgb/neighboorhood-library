8f61e060af3011e6a209d288f317b7a4c9cdd5e6
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-17 13:24:16 -0400

    Created setters and getters.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
 
-public class Book {
+
+  public class Book {
+
+    private int id;
+    private String isbn, title, checkedOutTo;
+    private boolean isCheckedOut;
+
+    public Book(boolean isCheckedOut, String checkedOutTo, String title, String isbn, int id) {
+          this.isCheckedOut = isCheckedOut;
+          this.checkedOutTo = checkedOutTo;
+          this.title = title;
+          this.isbn = isbn;
+          this.id = id;
+      }
+      public void checkOut(String name) {
+          this.checkedOutTo = name;
+          this.isCheckedOut = true;
+
 }
+      public void checkIn() {
+          this.checkedOutTo = "";
+          this.isCheckedOut = false;
+      }
+  }
+
+
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id
+}
+
+
+public boolean isCheckedOut() {
+    return isCheckedOut;
+}
+
+public void setCheckedOut(boolean checkedOut) {
+    isCheckedOut = checkedOut;
+}
+
+public String getCheckedOutTo() {
+    return checkedOutTo;
+}
+
+public void setCheckedOutTo(String checkedOutTo) {
+    this.checkedOutTo = checkedOutTo;
+}
+
+public String getTitle() {
+    return title;
+}
+
+public void setTitle(String title) {
+    this.title = title;
+}
+
+public String getIsbn() {
+    return isbn;
+}
+
+public void setIsbn(String isbn) {
+    this.isbn = isbn;
+}
+
+
+  public void setId(int id) {
+      this.id = id;
+  }
+  public int getId() {
+    return id;
+}
+
